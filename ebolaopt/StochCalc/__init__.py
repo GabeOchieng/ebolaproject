91774f96ca99b7373b5c560ce40f6ca257e842a6
Alta Fang <alta@princeton.edu>
2015-01-05 12:43:23 -0500

    Adding lists of variable names to StochCalc

--- ebolaopt/StochCalc/__init__.py
+++ ebolaopt/StochCalc/__init__.py
@@ -1,1 +1,7 @@
-#
+# pyModelParams variables
+ModelParamList = ["beta_I", "beta_H", "beta_F", "alpha", "gamma_h", "gamma_f", \
+                  "gamma_i", "gamma_d", "theta_1", "delta_1", "delta_2"]
+
+StochParamList = ["N_samples", "Trajectories", "I_init", "S_init", "H_init", \
+                  "F_init", "R_init", "E_init", "t_final"]
+
