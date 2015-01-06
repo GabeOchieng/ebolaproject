700f84aec215831f3d4a48ed74c374e28e1d201e
Yile Gu <yile@princeton.edu>
2015-01-05 22:49:02 -0500

    more tests

--- ebolaopt/tests/test_modelfit.py
+++ ebolaopt/tests/test_modelfit.py
@@ -9,8 +9,11 @@ from ebolaopt import fit_data
 class TestOpt(unittest.TestCase):
     
     def test1(self):
-        """Test optimize user interface."""
-        fit_data("ebolaopt/data/case_counts.csv", "Liberia", 10000)
-
+        """Test Different Countries"""
+        fit_data("ebolaopt/data/case_counts.csv", "Liberia", 1e7, plot = False)
+        fit_data("ebolaopt/data/case_counts.csv", "Sierra Leone", 1e7, plot = False)
+    def test2(self):
+        """Test Plots."""
+        fit_data("ebolaopt/data/case_counts.csv", "Sierra Leone", 1e7, plot = True)
 if __name__ == '__main__':
     unittest.main()
